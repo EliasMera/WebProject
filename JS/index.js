43522a790c410808675363f45c4c3475281d9c62
RogerMoon <rogerluna95@hotmail.com>
2017-04-16 18:03:32 -0500

    funcionalidad de list all

--- JS/index.js
+++ JS/index.js
@@ -35,6 +35,44 @@ $(document).ready(function () {
 	});
 
 
+	$("#btnListall").click(function(){
+	$("#resDiv").html("");
+	var jsonToSend = {
+		"action" : "loadNResults"
+	}
+
+	$.ajax({
+            url: "PHP/appLayer.php",
+            type: "POST",
+            data: jsonToSend,
+            dataType: "json",
+            contentType: "application/x-www-form-urlencoded",
+            success: function(jsonResponse){
+                console.log("wuu cargue cosas");
+                var newHtml = "";
+                for(i = 0; i < Math.min(10, jsonResponse.length); i++){
+                	var email = jsonResponse[i].email;
+                	var emailOwner = "<a href='mailto:" + email + "?Subject=Housing%20Question' target='_top'>" + jsonResponse[i].owner + "</a>";
+                	var renta = (jsonResponse[i].renta === "Y") ? " For Rent " : "";
+                	var venta = (jsonResponse[i].venta === "Y") ? " For Sell " : "";
+                	newHtml+= "<table>" + "<tr>" + "<th>" +"<img src='Images/" + jsonResponse[i].imagen + "' width='200' height='110'/>" + "</th>" +
+                   	"<td>" + jsonResponse[i].titulo + "<br>" + jsonResponse[i].direccion + "<br>" +
+                   	jsonResponse[i].descripcion + "<br>" + venta + " " + renta + "<br>" +
+                   	jsonResponse[i].property + "<br>" +
+                   	jsonResponse[i].precio + "  Contact: " + emailOwner + "</td>" + "</tr>";
+                }
+                newHtml += "</table>";
+                $("#resDiv").append(newHtml);
+            },
+            error: function(errorMessage){
+                console.log("failed");
+
+            }		
+	});
+
+	});
+
+
 	$("#btnRefresh").click(function () {
 		$("#resDiv").html("");
 		var min = ($("#from").val())? $("#from").val() : -9223372036854775807;
