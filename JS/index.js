e15b4f95009b458c5fd53b837c2e0cc3f27d45ed
RogerMoon <rogerluna95@hotmail.com>
2017-04-16 16:40:09 -0500

    arregle un bug

--- JS/index.js
+++ JS/index.js
@@ -73,6 +73,7 @@ $(document).ready(function () {
                 $("#resDiv").append(newHtml);
             },
             error: function(errorMessage){
+            	$("#resDiv").append("No matches found");
                 console.log("failed");
 
             }		
